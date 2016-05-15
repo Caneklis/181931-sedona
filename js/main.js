a69659d3cc9808c6cb2ec5d842b25eb66805eb0b
Caneklis <canek.lis@gmail.com>
2016-05-15 19:32:49 +0300

    Внес изменения

---
+++ js/main.js
@@ -0,0 +1,9 @@
+(function(){
+var link = document.querySelector(".booking-button");
+var popup = document.querySelector(".booking-form");
+link.addEventListener("click", function(event) {
+event.preventDefault();
+popup.classList.add("booking-form-show");
+close.addEventListener("click", function(event) {
+event.preventDefault();
+});
